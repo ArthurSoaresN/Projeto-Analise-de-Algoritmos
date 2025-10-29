880f9a8b1215801bd94a51db4924b659000b7fca
Arthur Soares <arthursoaressi@hotmail.com>
2025-10-28 23:37:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,36 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+#define CODE_LENGTH 12
+#define CNPJ_LENGTH 20
 
-char code;
-char real_cnpj;
-char received_cnpj;
+typedef struct {
+char code[CODE_LENGTH];
+char real_cnpj[CNPJ_LENGTH];
+char received_cnpj[CNPJ_LENGTH];
 char real_weight;
 char received_weight;
 int test_cnpj; // CNPJ correto = 1, CNPJ errado = 0 - variavel para ordenar por prioridade
 int test_weight; // Peso =< limite = 1, ultrapassou mais de 10% do limite (real_weight) = 0
 int id; // Ordem de cadastro
-
 } container;
 
-void read_file(const char* filename) {
 
-    int size_list = NULL;
-
-    FILE* file = fopen(filename, "r");
-    if (!file) {
-        perror("fopen");
-        return;
+int find_code(container* list, int size, const char* code_find) {
+    for (int i = 0; i < size; i++) {
+        if (strcmp(list[i].code, code_find) == 0) {
+            return i;
+        }
     }
+    return -1;
+}
 
-    if (size_list > 1) {
-        container container_list = [size_list];
-    }
 
-    // ler o arquivo aqui...
 
-    fclose(file);
+container* read_file(const char* filename, int* listsize_p) {
+
+
 }
 
 
