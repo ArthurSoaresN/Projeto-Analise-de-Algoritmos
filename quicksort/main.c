2f271ff037b2742d283c2f1b16449bade461aea1
Arthur Soares <arthursoaressi@hotmail.com>
2025-11-05 01:25:16 -0300

    Update main.c

--- quicksort/main.c
+++ quicksort/main.c
@@ -3,4 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ACRONYM 2
+
+typedef struct {
+    char name_quick[ACRONYM];
+    int performance;
+} results;
+
+void read_file () {}
+
+
+
+
+
+
+
+
+
+
+
 int main (int argc, char *argv[]) {}
