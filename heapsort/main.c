f5fca1cc4e85f263d116b24614621912de81234a
Arthur Soares <arthursoaressi@hotmail.com>
2025-11-09 15:47:01 -0300

    Update main.c

--- heapsort/main.c
+++ heapsort/main.c
@@ -3,6 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+    int prioridade;
+    unsigned char* dados;
+    int tamanho_dados;
+} Pacote;
+
+
+
+void heapify(Pacote* vetor, uint32_t T, uint32_t i) {
+    uint32_t P = i;
+    uint32_t E = 2 * i + 1;
+    uint32_t D = 2 * i + 2;
+
+    if (E < T && vetor[E].prioridade > vetor[P].prioridade) P = E; 
+    if (D < T && vetor[D].prioridade > vetor[P].prioridade) P = D;
+
+    if (P != i) {
+        Pacote temp = vetor[i];
+        vetor[i] = vetor[P];
+        vetor[P] = temp;
+        
+        heapify(vetor, T, P);
+    }
+}
+
+
+
+
+
 int main (int argc, char *argv[]) {
 
     return 0;
