4eee6a2724892fff80d48a676fb3fa219a6494ae
Arthur Soares <arthursoaressi@hotmail.com>
2025-12-11 01:04:58 -0300

    Update main.c

--- sequenciamento/main.c
+++ sequenciamento/main.c
@@ -4,9 +4,8 @@
 #include <string.h>
 #include <math.h>
 
-#define MAX_NOME 10
+#define MAX_NOME 12
 #define MAX_DNA 1000005 
-#define MAX_GENE 1000005
 
 typedef struct {
     char nome[MAX_NOME];
@@ -14,11 +13,90 @@ typedef struct {
     int id;
 } Doenca;
 
+Doenca* read_file(const char* filename, int* qtd_doencas_out);
+void computar_tabela_lps(char* padrao, int M, int* lps);
+int busca_kmp(char* texto, char* padrao);
+void merge(Doenca* arr, int l, int m, int r);
+void mergesort_doencas(Doenca* arr, int l, int r);
+
+Doenca* read_file(const char* filename, int* qtd_doencas_out) {
+    FILE* file = fopen(filename, "r");
+    if (!file) return NULL;
+
+    int K; 
+    if (fscanf(file, "%d\n", &K) != 1) return NULL;
+
+    char* sequenciamento = (char*)malloc(MAX_DNA * sizeof(char));
+    if (!sequenciamento) return NULL;
+    
+    fscanf(file, "%s\n", sequenciamento);
+
+    int numero_doencas = 0;
+    fscanf(file, "%d\n", &numero_doencas);
+    *qtd_doencas_out = numero_doencas;
+
+    Doenca* lista_doencas = (Doenca*)malloc(numero_doencas * sizeof(Doenca));
+
+    for (int i = 0; i < numero_doencas; i++) {
+        int numero_genes;
+        fscanf(file, "%s %d", lista_doencas[i].nome, &numero_genes);
+        lista_doencas[i].id = i; 
+        int genes_detectados = 0; 
+
+        for (int g = 0; g < numero_genes; g++) {
+            char gene_atual[1005]; 
+            fscanf(file, "%s", gene_atual);
+
+            int len_gene = strlen(gene_atual);
+            int total_subcadeias = len_gene - K + 1;
+
+            if (total_subcadeias > 0) {
+                int* coberto = (int*)calloc(len_gene, sizeof(int));
+                
+                char* sub = (char*)malloc((K + 1) * sizeof(char));
+                
+                for (int x = 0; x < total_subcadeias; x++) {
+                    strncpy(sub, &gene_atual[x], K);
+                    sub[K] = '\0'; 
+                    
+                    if (busca_kmp(sequenciamento, sub)) {
+                        for (int k = 0; k < K; k++) {
+                            coberto[x + k] = 1;
+                        }
+                    }
+                }
+                free(sub);
+
+                int total_cobertos = 0;
+                for (int c = 0; c < len_gene; c++) {
+                    total_cobertos += coberto[c];
+                }
+                free(coberto);
+
+                double compatibilidade = ((double)total_cobertos / len_gene) * 100.0;
+                
+                if (compatibilidade >= 90.0) {
+                    genes_detectados++;
+                }
+            }
+        }
+
+        if (numero_genes > 0)
+            lista_doencas[i].probabilidade = ((double)genes_detectados / numero_genes) * 100.0;
+        else
+            lista_doencas[i].probabilidade = 0.0;
+    }
+    
+    free(sequenciamento); 
+    fclose(file);
+    return lista_doencas;
+}
+
 void computar_tabela_lps(char* padrao, int M, int* lps) {
     int len = 0;
     lps[0] = 0;
-
-    for (int i = 1; i < M; ) { 
+    int i = 1;
+    while (i < M) {
         if (padrao[i] == padrao[len]) {
             len++;
             lps[i] = len;
@@ -66,116 +144,6 @@ int busca_kmp(char* texto, char* padrao) {
     return encontrou;
 }
 
-Doenca* read_file(const char* filename, int* qtd_doencas_out) {
-    FILE* file = fopen(filename, "r");
-    if (!file) return NULL;
-
-    int K; 
-    if (fscanf(file, "%d\n", &K) != 1) return NULL;
-
-    char* sequenciamento = (char*)malloc(MAX_DNA * sizeof(char));
-    if (!sequenciamento) return NULL;
-    
-    if (fscanf(file, "%s\n", sequenciamento) != 1) {
-        free(sequenciamento);
-        fclose(file);
-        return NULL;
-    }
-
-    int numero_doencas = 0;
-    if (fscanf(file, "%d\n", &numero_doencas) != 1) {
-        free(sequenciamento);
-        fclose(file);
-        return NULL;
-    }
-    *qtd_doencas_out = numero_doencas;
-
-    Doenca* lista_doencas = (Doenca*)malloc(numero_doencas * sizeof(Doenca));
-    if (!lista_doencas) {
-        free(sequenciamento);
-        fclose(file);
-        return NULL;
-    }
-
-    for (int i = 0; i < numero_doencas; i++) {
-        int numero_genes;
-        if (fscanf(file, "%s %d", lista_doencas[i].nome, &numero_genes) != 2) {
-            free(sequenciamento);
-            free(lista_doencas);
-            fclose(file);
-            return NULL;
-        }
-        lista_doencas[i].id = i; 
-        int genes_detectados = 0; 
-
-        for (int g = 0; g < numero_genes; g++) {
-            char* gene_atual = (char*)malloc(MAX_GENE * sizeof(char));
-            if (!gene_atual) {
-                free(sequenciamento);
-                free(lista_doencas);
-                fclose(file);
-                return NULL;
-            }
-            
-            if (fscanf(file, "%s", gene_atual) != 1) {
-                free(gene_atual);
-                free(sequenciamento);
-                free(lista_doencas);
-                fclose(file);
-                return NULL;
-            }
-
-            int len_gene = strlen(gene_atual);
-            
-            if (len_gene >= K) {
-                int total_subcadeias = len_gene - K + 1;
-                int subcadeias_encontradas = 0;
-
-                for (int x = 0; x < total_subcadeias; x++) {
-                    char* sub = (char*)malloc((K + 1) * sizeof(char));
-                    if (!sub) {
-                        free(gene_atual);
-                        free(sequenciamento);
-                        free(lista_doencas);
-                        fclose(file);
-                        return NULL;
-                    }
-                    
-                    strncpy(sub, &gene_atual[x], K);
-                    sub[K] = '\0';
-                    
-                    if (busca_kmp(sequenciamento, sub)) {
-                        subcadeias_encontradas++;
-                    }
-                    
-                    free(sub);
-                }
-
-                double compatibilidade = 0.0;
-                if (total_subcadeias > 0) {
-                    compatibilidade = ((double)subcadeias_encontradas / total_subcadeias) * 100.0;
-                }
-                
-                if (compatibilidade >= 90.0) {
-                    genes_detectados++;
-                }
-            }
-            
-            free(gene_atual);
-        }
-
-        if (numero_genes > 0) {
-            lista_doencas[i].probabilidade = ((double)genes_detectados / numero_genes) * 100.0;
-        } else {
-            lista_doencas[i].probabilidade = 0.0;
-        }
-    }
-    
-    free(sequenciamento);
-    fclose(file);
-    return lista_doencas;
-}
-
 void merge(Doenca* arr, int l, int m, int r) {
     int i, j, k;
     int n1 = m - l + 1;
@@ -183,8 +151,6 @@ void merge(Doenca* arr, int l, int m, int r) {
 
     Doenca* L = (Doenca*)malloc(n1 * sizeof(Doenca));
     Doenca* R = (Doenca*)malloc(n2 * sizeof(Doenca));
-    
-    if (!L || !R) exit(1);
 
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
@@ -195,8 +161,8 @@ void merge(Doenca* arr, int l, int m, int r) {
     j = 0; 
     k = l; 
     while (i < n1 && j < n2) {
-        long probA = round(L[i].probabilidade);
-        long probB = round(R[j].probabilidade);
+        long probA = lround(L[i].probabilidade);
+        long probB = lround(R[j].probabilidade);
 
         if (probA > probB) {
             arr[k] = L[i];
@@ -258,15 +224,10 @@ int main(int argc, char* argv[]) {
     mergesort_doencas(lista_doencas, 0, numero_doencas - 1);
 
     FILE* outfile = fopen(argv[2], "w");
-    if (!outfile) {
-        free(lista_doencas);
-        return 1;
-    }
+    if (!outfile) return 1;
 
     for (int i = 0; i < numero_doencas; i++) {
-        fprintf(outfile, "%s->%.0f%%\n", 
-                lista_doencas[i].nome, 
-                round(lista_doencas[i].probabilidade));
+        fprintf(outfile, "%s->%ld%%\n", lista_doencas[i].nome, lround(lista_doencas[i].probabilidade));
     }
 
     fclose(outfile);
